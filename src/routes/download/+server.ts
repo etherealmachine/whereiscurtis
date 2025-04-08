cf3ee6ae3f399c81faa67994f03499d83d94c0eb
James Pettit <etherealmachine@gmail.com>
2025-04-07 21:21:42 -0700

    Simplify download

--- src/routes/download/+server.ts
+++ src/routes/download/+server.ts
@@ -1,29 +1,14 @@
-import { getEvents, getLastApiRequestInfo, initializeDatabase, storeEvents } from "$lib/database";
-import { latestSpotMessages, type SpotMessage } from "$lib/spot_api";
+import { getEvents, initializeDatabase } from "$lib/database";
 import type { RequestEvent } from "@sveltejs/kit";
 
 export async function GET({ params }: RequestEvent): Promise<Response> {
   await initializeDatabase();
-  const { time: lastApiRequestTime, status: lastApiResponseStatus } = await getLastApiRequestInfo();
-  let messages: SpotMessage[] = [];
-  let fromCache = false;
-  if (lastApiRequestTime === null || lastApiRequestTime < Date.now() - 5 * 60 * 1000) {
-    messages = await latestSpotMessages();
-    await storeEvents(messages);
-  } else {
-    messages = await getEvents();
-    fromCache = true;
-  }
+  const messages = await getEvents();
 
   const date = new Date().toISOString().split('T')[0];
   const filename = `whereiscurtis_backup_${date}.json`;
   
-  return new Response(JSON.stringify({
-    messages,
-    lastApiRequestTime,
-    lastApiResponseStatus,
-    fromCache,
-  }), {
+  return new Response(JSON.stringify(messages), {
     headers: {
       'Content-Type': 'application/json',
       'Content-Disposition': `attachment; filename="${filename}"`
