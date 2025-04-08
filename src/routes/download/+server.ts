08229b5beca382e19a5aa3d920060760e02bcf63
James Pettit <etherealmachine@gmail.com>
2025-04-07 21:34:33 -0700

    Return database file directly if asked

--- src/routes/download/+server.ts
+++ src/routes/download/+server.ts
@@ -1,17 +1,34 @@
-import { getEvents, initializeDatabase } from "$lib/database";
+import { env } from "$env/dynamic/private";
+import { getEvents, initializeDatabase, getDb } from "$lib/database";
 import type { RequestEvent } from "@sveltejs/kit";
+import { readFileSync } from 'fs';
 
-export async function GET({ params }: RequestEvent): Promise<Response> {
+export async function GET({ url }: RequestEvent): Promise<Response> {
+  const date = new Date().toISOString().split('T')[0];
+  const fileprefix= `whereiscurtis_backup_${date}`;
+  
+  if (url.searchParams.get('raw')) {
+    try {
+      const dbPath = env.DATABASE_URL as string;
+      const file = readFileSync(dbPath);
+      return new Response(file, {
+        headers: {
+          'Content-Type': 'application/x-sqlite3',
+          'Content-Disposition': `attachment; filename="${fileprefix}.sqlite3"`
+        }
+      });
+    } catch (error) {
+      console.error('Error reading database file:', error);
+      return new Response('Error reading database file', { status: 500 });
+    }
+  }
+
   await initializeDatabase();
   const messages = await getEvents();
-
-  const date = new Date().toISOString().split('T')[0];
-  const filename = `whereiscurtis_backup_${date}.json`;
-  
   return new Response(JSON.stringify(messages), {
     headers: {
       'Content-Type': 'application/json',
-      'Content-Disposition': `attachment; filename="${filename}"`
+      'Content-Disposition': `attachment; filename="${fileprefix}.json"`
     }
   });
 }
