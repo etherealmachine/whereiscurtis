7f92bf87a7d756e9061230a94cfa683b75f0ae8d
James Pettit <etherealmachine@gmail.com>
2025-04-27 12:14:10 -0700

    Try setting up schedules

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.hardRefresh = event.request.headers.get('Cache-Control') === 'no-cache';
-	const response = await resolve(event);
-	return response;
-};
